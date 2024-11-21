6b894e3c1dfaa6266230804cb87f902a12f9c8c1
Aruna Murugesan <arunax777@gmail.com>
2024-11-21 12:55:29 -0600

    initial setup

---
+++ src/main/java/com/example/Hotel_booking_backend/HotelBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hotel_booking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Hotel_booking_backend/HotelBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hotel_booking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
