b8e6e3d63cf6da333650dbdbf5762a3665c15431
Aruna Murugesan <arunax777@gmail.com>
2024-11-25 16:23:07 -0600

    JWT Setup

--- Authentication-Service/src/main/java/com/example/Authentication_Service/security/JwtUtil.java
+++ Authentication-Service/src/main/java/com/example/Authentication_Service/security/JwtUtil.java
@@ -1,4 +1,36 @@
 package com.example.Authentication_Service.security;
 
+import com.example.Authentication_Service.model.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
 public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private static final long EXPIRATION_TIME = 3600000;
+
+    public String generateToken(User user) {
+
+        byte[] keyBytes = secretKey.getBytes();
+        SecretKey key = Keys.hmacShaKeyFor(keyBytes);
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("id", user.getId())
+                .claim("firstName", user.getFirstName())
+                .claim("lastName", user.getLastName())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
 }
